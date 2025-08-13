fc6fa21efbf0f4ba97f63ad563b3db8f38b9c220
joshuakitong <joshuakitong@gmail.com>
2025-08-13 20:23:31 +0800

    Initial commit for QuizifAI

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-yellow-300">QuizifAI</h1>
+    </div>
+  );
+}
+
+export default App;
